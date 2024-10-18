52cdd61a5005052b00663856c91bf2f702d64244
Tom Jenkinson <tom.jenkinson@redhat.com>
2024-10-18 10:55:40 +0100

    Discussion with Marco

--- extensions/narayana-jta/runtime/src/test/java/io/quarkus/narayana/jta/runtime/NarayanaJtaRecorderTest.java
+++ extensions/narayana-jta/runtime/src/test/java/io/quarkus/narayana/jta/runtime/NarayanaJtaRecorderTest.java
@@ -1,9 +1,9 @@
 package io.quarkus.narayana.jta.runtime;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,25 +13,37 @@ public class NarayanaJtaRecorderTest {
     void testByteLength() {
         TransactionManagerConfiguration transactions = new TransactionManagerConfiguration();
         transactions.shortenNodeNameIfNecessary = true;
-        // create nodeNames larger than 28 bytes
-        for (int i = 1; i < 100; i++) {
-            String originalNodeName = UUID.randomUUID().toString();
-            NarayanaJtaRecorder r = new NarayanaJtaRecorder();
-            transactions.nodeName = originalNodeName;
-            r.setNodeName(transactions);
-            int numberOfBytes = transactions.nodeName.getBytes(StandardCharsets.UTF_8).length;
-            assertFalse(numberOfBytes > 28,
-                    "node name bytes still exceeded 28 bytes limit, number of bytes is " + numberOfBytes);
-        }
-        for (int i = 1; i < 1000; i++) {
-            byte[] data = new byte[i];
-            NarayanaJtaRecorder r = new NarayanaJtaRecorder();
-            transactions.nodeName = new String(data);
-            r.setNodeName(transactions);
-            int numberOfBytes = transactions.nodeName.getBytes(StandardCharsets.UTF_8).length;
-            assertFalse(numberOfBytes > 28,
-                    "node name bytes still exceeded 28 bytes limit, number of bytes is " + numberOfBytes);
-        }
+        String originalNodeName = "dfe2420d-b12e-4ec3-92c0-ee7c4";
+        assertTrue(originalNodeName.getBytes(StandardCharsets.UTF_8).length > 28);
+        NarayanaJtaRecorder r = new NarayanaJtaRecorder();
+        transactions.nodeName = originalNodeName;
+        r.setNodeName(transactions);
+        int numberOfBytes = transactions.nodeName.getBytes(StandardCharsets.UTF_8).length;
+        assertEquals(28, numberOfBytes,
+                "node name bytes was not 28 bytes limit, number of bytes is " + numberOfBytes);
+    }
+
+    @Test
+    void testPredictableConversion() {
+        TransactionManagerConfiguration transactions = new TransactionManagerConfiguration();
+        transactions.shortenNodeNameIfNecessary = true;
+        String originalNodeName = "dfe2420d-b12e-4ec3-92c0-ee7c4";
+        assertTrue(originalNodeName.getBytes(StandardCharsets.UTF_8).length > 28);
+        NarayanaJtaRecorder r = new NarayanaJtaRecorder();
+        transactions.nodeName = originalNodeName;
+        r.setNodeName(transactions);
+        int numberOfBytes = transactions.nodeName.getBytes(StandardCharsets.UTF_8).length;
+        assertEquals(28, numberOfBytes,
+                "node name bytes was not 28 bytes limit, number of bytes is " + numberOfBytes);
+        String firstConversion =transactions.nodeName;
+        transactions.nodeName = originalNodeName;
+        r.setNodeName(transactions);
+        String secondConversion = transactions.nodeName;
+        numberOfBytes = transactions.nodeName.getBytes(StandardCharsets.UTF_8).length;
+        assertEquals(28, numberOfBytes,
+                "node name bytes was not 28 bytes limit, number of bytes is " + numberOfBytes);
+        assertEquals(firstConversion, secondConversion,
+                "Node names were shortened differently: " + firstConversion + " " + secondConversion);
     }
 
 }
